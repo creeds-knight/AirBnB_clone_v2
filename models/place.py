6947a09b6757e9e568e5b5bbc868cee88e83d8ef
creeds-knight <apedoarthur21@gmail.com>
2024-03-19 17:55:49 +0300

    Task-10, Intial commit

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Float, Integer
+from sqlalchemy import Column, String, ForeignKey, Float, Integer, Table
 import os
 from sqlalchemy.orm import relationship
 
@@ -9,6 +9,12 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
+
+    place_amenity = Table('place_amenity', Base.metadata,
+            Column('place_id', String(60), ForeignKey('places.id'),
+                    primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                    primary_key=True, nullable=False))
 
     env = os.getenv('HBNB_TYPE_STORAGE')
     if env == "db":
@@ -26,6 +32,8 @@ class Place(BaseModel, Base):
 
         reviews = relationship("Review", cascade="all, delete, delete-orphan",
                                backref="place")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False)
 
     @property
     def reviews(self):
@@ -38,5 +46,17 @@ class Place(BaseModel, Base):
         for k, v in all_reviews:
             if v.get('place_id') == Place.id:
                 reviews.append(v)
-
         return reviews
+
+    @property
+    def amenities(self):
+        """Returns a list of amenities based on amenity_id """
+        from models import storage
+        from models.amenity import Amenity
+        all_amenities = storage.all(Amenity)
+        amenity_lst = []
+        for k, v in all_amenities:
+            if v.get('place_id') == Place.id:
+                amenity_lst.append(v)
+        return amenity_lst
+
