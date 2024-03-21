9c67f483d2c1832f6d9cbf634064a52792aaf125
creeds-knight <apedoarthur21@gmail.com>
2024-03-21 13:36:33 +0300

    task 10 removal

--- models/amenity.py
+++ models/amenity.py
@@ -9,5 +9,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
-    place_amenity = relationship('Place', secondary='place_amenity',
-            back_populates='amenities')
+    #place_amenity = relationship('Place', secondary='place_amenity',
+     #       back_populates='amenities')
--- models/place.py
+++ models/place.py
@@ -10,11 +10,11 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
 
-    place_amenity = Table('place_amenity', Base.metadata,
+    """place_amenity = Table('place_amenity', Base.metadata,
             Column('place_id', String(60), ForeignKey('places.id'),
                     primary_key=True, nullable=False),
             Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                    primary_key=True, nullable=False))
+                    primary_key=True, nullable=False))"""
 
     env = os.getenv('HBNB_TYPE_STORAGE')
     if env == "db":
@@ -32,7 +32,7 @@ class Place(BaseModel, Base):
 
         reviews = relationship("Review", cascade="all, delete, delete-orphan",
                                backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
+        #amenities = relationship("Amenity", secondary="place_amenity",
                                  viewonly=False)
 
     @property
@@ -48,7 +48,7 @@ class Place(BaseModel, Base):
                 reviews.append(v)
         return reviews
 
-    @property
+    """@property
     def amenities(self):
         """Returns a list of amenities based on amenity_id """
         from models import storage
@@ -58,5 +58,5 @@ class Place(BaseModel, Base):
         for k, v in all_amenities:
             if v.get('place_id') == Place.id:
                 amenity_lst.append(v)
-        return amenity_lst
+        return amenity_lst"""
 
