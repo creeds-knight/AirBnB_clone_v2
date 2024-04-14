e997ec00924027957e5f30685afdb9e176d7492b
creeds-knight <apedoarthur21@gmail.com>
2024-04-14 08:48:48 +0300

    archiving web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python3
+""" A fabric script that archives the contents of web_static"""
 
 from fabric.api import run, env, local
 import os
 from datetime import datetime
 
+
 def do_pack():
     """ Generates a .tgz archive """
 
     if not os.path.exists('versions'):
-          os.mkdir('versions')
+        os.mkdir('versions')
 
     now = datetime.now()
     timestmp = now.strftime("%H%m%d%H%M%S")
@@ -16,7 +19,7 @@ def do_pack():
 
     result = local(command, capture=False)
 
-    if result.succeeded == True:
+    if result.succeeded is True:
         return os.path.join('versions', archive_name)
     else:
         return None
