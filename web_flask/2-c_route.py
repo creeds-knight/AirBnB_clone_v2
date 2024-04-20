ab936c78477827ba0004fe3a89e7dc156b2e151d
creeds-knight <apedoarthur21@gmail.com>
2024-04-20 11:48:40 +0300

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """ displays variable"""
-    return f"C {escape(text.replace(' ', '_'))}"
+    return f"C {escape(text.replace('_', ' '))}"
 
 
 if __name__ == "__main__":
