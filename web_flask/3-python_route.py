f7d9e071913b9c22d13d7e7faf47c749af2e57f1
creeds-knight <apedoarthur21@gmail.com>
2024-04-20 12:06:14 +0300

    task3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,5 +30,10 @@ def pythoniscool(text="is cool"):
     return f"Python {escape(text.replace('_', ' '))}"
 
 
+@app.route('/python', strict_slashes=False)
+def python():
+    """ returns python is cool"""
+    return " Python is cool"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
