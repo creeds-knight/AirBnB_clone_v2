4685ba5676d6d325be3ad3a626b476f08df78255
creeds-knight <apedoarthur21@gmail.com>
2024-04-21 12:23:05 +0300

    task7

--- models/state.py
+++ models/state.py
@@ -6,29 +6,32 @@ from sqlalchemy.orm import relationship
 import models
 from models.city import City
 import shlex
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
 
-    name = Column(String(128), nullable=False)
+    env = os.getenv("HBNB_TYPE_STORAGE")
+    if env == "db":
+        name = Column(String(128), nullable=False)
 
-    cities = relationship("City", cascade="all, delete, delete-orphan",
+        cities = relationship("City", cascade="all, delete, delete-orphan",
                           backref="state")
+    else:
+        @property
+        def cities(self):
+            """ Returns a list of city instances with state_id = State_id"""
+            all_data = models.storage.all()
+            cities = []
+            for keys in all_data:
+                city = keys.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    cities.append(all_data[keys])
 
-    @property
-    def cities(self):
-        """ Returns a list of city instances with state_id = State_id"""
-        all_data = models.storage.all()
-        cities = []
-        for keys in all_data:
-            city = keys.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                cities.append(all_data[keys])
-
-        for _ in cities:
-            if (_.state_id == self.id):
-                res.apppend(_)
-        return res
+            for _ in cities:
+                if (_.state_id == self.id):
+                    res.apppend(_)
+            return res
