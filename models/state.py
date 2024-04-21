68360fa10427346aabaf675b56f791c401a84ad9
creeds-knight <apedoarthur21@gmail.com>
2024-04-21 12:33:09 +0300

    webflask

--- models/state.py
+++ models/state.py
@@ -18,7 +18,7 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
 
         cities = relationship("City", cascade="all, delete, delete-orphan",
-                              backref="state")
+                          backref="state")
     else:
         @property
         def cities(self):
